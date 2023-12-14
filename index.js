5e3f4b93987eda7c1dd66760b8365f86475d6336
dhirajkalwar57 <dhirajkalwar57@gmail.com>
2023-12-14 13:06:31 +0530

    testing

--- index.js
+++ index.js
@@ -8,21 +8,29 @@ require('dotenv').config();
 const app = express();
 const router = express.Router();
 app.use(cors({
-    origin: 'https://cookie-testing-frontend.vercel.app',
+    // origin: 'https://cookie-testing-frontend.vercel.app',
+    origin: 'http://localhost:5174',
     credentials: true,
 }));
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Expose-Headers', 'jwtoken');
+    next();
+  });
 
 app.post('/setCookie', (req,res) => {
     console.log(req.body.token);
-    res.cookie('token',req.body.token,{
-        domain:'vercel.app',
-    });
+    // res.cookie('token',req.body.token,{
+    //     domain:'vercel.app',
+    // });
+    // res.setHeader('Set-Cookie', 'jwtoken=ejghqwertyuiopasdfghjklzxcvbnm34kjhdhiraj; max-age=86400; domain=localhost; path=/');
+    res.setHeader('jwtoken', 'ejghqwertyuiopasdfghjklzxcvbnm34kjhdhiraj');
     res.json({message:'ok'});
 })
 app.get('/sendCookie', (req,res) => {
     console.log(req.headers['jwt']);
+    console.log(req.headers['jwtoken']);
     console.log(req.cookies.token);
 })
 
